74e191f513883f932cfcc4f15815ddedfd92caeb
UnFrindLi <unfrindli@gmail.com>
2016-11-30 10:54:35 +0330

    Update 121212.lua

--- plugins/121212.lua
+++ plugins/121212.lua
@@ -1,20 +1,21 @@
 local function run(msg)
-if msg.text == "121212" then
-	return "121212"
+if msg.text == "[Mm][aeAE]hdi" then
+	return "hom"
 end
-if msg.text == "121212" then
-	return "121212"
+if msg.text == "مهدی" then
+	return "hom"
 end
 end
 return {
 	description = "Chat With Robot Server", 
 	usage = "chat with robot",
 	patterns = {
-		"^121212$",
-		
+		"^[Mm][aeAE][Hh][Dd][Ii]$",
+		"مهدی",
+				
 		}, 
 	run = run,
     --privileged = true,
 	pre_process = pre_process
 }
--- by @Iiborn
+
