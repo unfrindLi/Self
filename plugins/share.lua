0552d781d51a467d0b3f52f89e287cc55c18b0ab
UnFrindLi <unfrindli@gmail.com>
2016-12-01 16:44:46 +0330

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,12 +1,12 @@
 do
-
+-- need edit 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+989216358178", "#Aren", "", ok_cb, false)
+send_contact(get_receiver(msg), "+", "name", "Lname", ok_cb, false)
 end
 
 return {
 patterns = {
-"^!abbasnum$",
+"^share$",
 
 },
 run = run
