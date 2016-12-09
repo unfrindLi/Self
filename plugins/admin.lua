7ff510c5ac4e1488d0eda5eac05bbe8a7b1a503f
UnFrindLi <unfrindli@gmail.com>
2016-12-09 15:21:28 +0330

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -169,7 +169,18 @@ local function run(msg,matches)
     	redis:set("bot:photo", "waiting")
     	return 'Please send me bot photo now'
     end
-    if matches[1] == "markread" then
+    if matches[1] == "👁" then
+    	if matches[2] == "😴" then
+    		redis:set("bot:markread", "on")
+    		return "Mark read > on"
+    	end
+    	if matches[2] == "off" then
+    		redis:del("bot:markread")
+    		return "Mark read > off"
+    	end
+    	return
+    end
+	if matches[1] == "markread" then
     	if matches[2] == "on" then
     		redis:set("bot:markread", "on")
     		return "Mark read > on"
@@ -321,6 +332,7 @@ return {
 	"^[#!/](pmblock) (%d+)$",
 	"^[#!/](markread) (on)$",
 	"^[#!/](markread) (off)$",
+	"(👁) (😴)",
 	"^[#!/](setbotphoto)$",
 	"^[#!/](contactlist)$",
 	"^[#!/](dialoglist)$",
