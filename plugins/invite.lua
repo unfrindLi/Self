5d9972857c6adf19641cbc62f727167924f18189
UnFrindLi <unfrindli@gmail.com>
2016-12-09 15:35:45 +0330

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -5,13 +5,14 @@ local bot_id = our_id
 local receiver = get_receiver(msg)
     if matches[1] == 'leave' and is_admin1(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+            chaannel_del_user("channel#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     leave_channel(receiver, ok_cb, false)
+            sleep(5)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
        send_large_msg(receiver, ':|', ok_cb, false)
        chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
     leave_channel(receiver, ok_cb, false)
-            sleep(10)
-    end
+      end
 end
  local clock = os.clock
 function sleep(n)  -- seconds
